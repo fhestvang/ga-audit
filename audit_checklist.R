edb5c2b68cd3d4e944c62def154bff9efeef47cb
fh <frederikhestvang@gmail.com>
2019-06-17 15:24:06 +0200

    -a

--- audit_checklist.R
+++ audit_checklist.R
@@ -1,29 +1,31 @@
-
+# 
+# FETCHING DATA
+# Every bullet below is asking the GA API a question about the state of the implemetation
+# For a description, see: https://docs.google.com/spreadsheets/d/1bCL3eXcATDGloD-9pFodBMg-eBUuakMQdaZIpuRv7iE/edit#gid=988872659
+#
 
 fetch_data <- function(dim, met){
-  
   google_analytics(viewId = VIEW_ID, 
                        date_range = DATE_RANGE, 
                        dimensions = c(dim), 
                        metrics = met, 
                        anti_sample = TRUE)
-  
 }
 
-## Direct Traffic
+# DIRECT TRAFFIC 
 direct_traffic <- function(){
 df <- fetch_data("sourceMedium", "pageViews")
 df %<>% arrange(-pageViews) %>% mutate(share = pageViews/sum(pageViews)) %>% filter(sourceMedium == "(direct) / (none)")
 if (df$share > 0.15) {
-  print(paste0("Direct traffic is above average: ", percent(df$share), " of total traffic"))
-} else {
+    print(paste0("Direct traffic is above average: ", percent(df$share), " of total traffic"))
+    } else {
   "Low level"
-}
+  }
 }
 
 direct_traffic()
 
-## BounceRate check
+## BOUNCERATE
 bounce_rate <- function() {
   df <- fetch_data(c("landingPagePath", "sourceMedium"), c("bounceRate", "pageViews"))
   df %<>% mutate(share = pageViews/sum(pageViews)) %>% filter(share > 0.001) %>% arrange(-pageViews)
--- variables.R
+++ variables.R
@@ -8,13 +8,13 @@ library(httr)
 # source("data_fetching.R")
 
 ## VARIABLES
-SS_NAME = "Hello"
+SS_NAME = "Biites Audit"
 SS_ACCOUNT_TITLE = "Account Overview"
 SS_QUERYLIST_TITLE = "Query Parameters"
-ACCOUNT_ID <- "18036718"
+ACCOUNT_ID <- "79746685"
 PROPERTY_ID <- paste0("UA-", ACCOUNT_ID, "-4")
-VIEW_ID <- "98075070"
-DATE_RANGE <- c("2019-06-01", "2019-06-02")
+VIEW_ID <- "134235444"
+DATE_RANGE <- c("2019-06-01", "2019-06-15")
 
 ## AUTHENTICATION
 ga_auth()
